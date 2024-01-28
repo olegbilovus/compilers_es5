50f5a6661a325a73802ce045fd80fafd903f8647
Oleg Bilovus <68842946+OB-UNISA@users.noreply.github.com>
2024-01-28 13:04:57 +0100

    update CI

--- src/main/esercitazione5/Main.java
+++ src/main/esercitazione5/Main.java
@@ -17,21 +17,24 @@ public class Main {
   public static void main(String[] args) throws Exception {
     File fileInput = new File(args[0]);
     Reader reader = new StringReader(Files.readString(fileInput.toPath()));
+    try {
+      Yylex lexer = new Yylex(reader);
+      parser p = new parser(lexer);
+      ProgramOP ast = (ProgramOP) p.parse().value;
+      StringTable st = lexer.getStringTable();
 
-    Yylex lexer = new Yylex(reader);
-    parser p = new parser(lexer);
-    ProgramOP ast = (ProgramOP) p.parse().value;
-    StringTable st = lexer.getStringTable();
+      ast.accept(new SemanticVisitor(st));
+      ast.accept(new ScopingVisitor(st));
+      ast.accept(new TypeCheckVisitor(st));
+      String generatedC = ast.accept(new GenCVisitor(st));
 
-    ast.accept(new SemanticVisitor(st));
-    ast.accept(new ScopingVisitor(st));
-    ast.accept(new TypeCheckVisitor(st));
-    String generatedC = ast.accept(new GenCVisitor(st));
-
-    String fileName = Paths.get(args[0]).getFileName().toString().split("\\.")[0];
-    try (FileWriter fileWriter = new FileWriter(
-        "test_files" + File.separator + "c_out" + File.separator + fileName + ".c")) {
-      fileWriter.write(generatedC);
+      String fileName = Paths.get(args[0]).getFileName().toString().split("\\.")[0];
+      try (FileWriter fileWriter = new FileWriter(
+          "test_files" + File.separator + "c_out" + File.separator + fileName + ".c")) {
+        fileWriter.write(generatedC);
+      }
+    } catch (Exception e) {
+      System.out.println(Paths.get(args[0]).getFileName().toString());
     }
   }
 
